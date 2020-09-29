d2e6dfaedd16a5e1a3273bd7067b37d57681b071
Thet Hmuu <thethmuueinsoe@gmail.com>
2020-09-29 16:30:32 +0630

    Task Create Function

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,49 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:notice] = "Task Added"
+      redirect_to task_path(@task.id)
+    else
+      render :new
+    end
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+
+  end
+
+  def destroy
+    if @task.destroy
+      flash[:notice] = "Deleted Successfully"
+      redirect_to tasks_path
+    end
+  end
+
+  private
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :deadline, :status, :priority)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_073927) do
+ActiveRecord::Schema.define(version: 2020_09_29_083633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "details"
+    t.string "deadline"
+    t.string "status"
+    t.string "priority"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
