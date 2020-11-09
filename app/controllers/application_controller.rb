ac961558788211726f5f007390ba8f90055e82fe
Thet Hmuu Eain Soe <thethmuueinsoe@gmail.com>
2020-11-09 14:57:43 +0630

    Restrict Access to Unauthenicated User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   def authenticate_user
-    if @current_user == nil
+    if current_user == nil
       flash[:notice] = 'You Need to Log In or Sign Up before Continuing'
       redirect_to new_session_path
     end
